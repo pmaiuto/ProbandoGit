82f038a643a3af7b9549511893e794121d171706
pablo <pablo@pablo-PC.home>
2017-03-25 14:11:28 -0300

    primer cambio

---
+++ ProbandoGit/src/paquete/git/Prueba.java
@@ -0,0 +1,13 @@
+package paquete.git;
+
+public class Prueba {
+private String nombre;
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+}
